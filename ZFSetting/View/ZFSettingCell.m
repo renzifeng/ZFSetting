f77ab6ad808c0fe77984bde9f2f96421230fa02a
bqlin <bqlins@163.com>
2018-04-01 18:07:58 +0800

    修正 ZFSettingItemTypeArrow 类型 cell 刷新时没有把上一次的 accessoryView 清空导致的显示不正确问题

--- ZFSetting/View/ZFSettingCell.m
+++ ZFSetting/View/ZFSettingCell.m
@@ -42,27 +42,24 @@
 - (void)setItem:(ZFSettingItem *)item {
     _item = item;
     // 设置数据
-	self.imageView.image = item.icon.length ? [UIImage imageNamed:item.icon] : nil;
+    self.imageView.image = item.icon.length ? [UIImage imageNamed:item.icon] : nil;
     self.textLabel.text = item.title;
-    if (item.type == ZFSettingItemTypeArrow) {
-        self.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        // 用默认的选中样式
-        self.selectionStyle = UITableViewCellSelectionStyleBlue;
-    } else if (item.type == ZFSettingItemTypeSwitch) {
+    if (item.type == ZFSettingItemTypeSwitch) {
         if (_switch == nil) {
             _switch = [[UISwitch alloc] init];
             _switch.on = item.switchOn;
             [_switch addTarget:self action:@selector(switchStatusChanged:) forControlEvents:UIControlEventValueChanged];
-	} else {
-	    [_switch setOn:item.switchOn animated:YES];
-	}
+        } else {
+            [_switch setOn:item.switchOn animated:YES];
+        }
         // 右边显示开关
         self.accessoryView = _switch;
         // 禁止选中
         self.selectionStyle = UITableViewCellSelectionStyleNone;
     } else {
-        // 什么也没有，清空右边显示的view
         self.accessoryView = nil;
+        self.accessoryType = item.type == ZFSettingItemTypeArrow ?
+        UITableViewCellAccessoryDisclosureIndicator : UITableViewCellAccessoryNone;
         // 用默认的选中样式
         self.selectionStyle = UITableViewCellSelectionStyleBlue;
     }
